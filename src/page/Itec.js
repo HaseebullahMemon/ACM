2c63ce13037d0a332495a60a6978ca7892db0691
Haseebullah <haseebullahmemon33@gmail.com>
2022-07-09 11:28:51 +0500

    itect/banner

---
+++ src/page/Itec.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Itec = () => {
+  return (
+    <div>
+        
+         <div>
+    <section id="home">
+  <div className="container-fluid px-0 top-banner">
+  <div className="container">
+      <div className="row">
+  <div className="col-lg-5 col-md-6">
+<h1>Good food choices are good investments</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias, quisquam?</p>
+  </div>
+<div className="mt-4">
+  <button className="main-btn">Order new <i className='fas fa-shopping-basket'></i> </button>
+  <button className='white-btn ms-lg-4 mt-lg-0 mt-4'>Order Now<i className='fas fa-angle-right ps-3'></i></button>
+</div>
+      </div>
+  </div>
+  </div>
+    </section>
+  </div></div>
+  )
+}
+
+export default Itec
